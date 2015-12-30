3dd430a2c1b2932c45965412aa63f4068ca01808
Sérgio A. Kopplin <sergiokopplin@gmail.com>
2015-12-30 11:39:27 -0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,27 +10,4 @@ download() {
     fi
 }
 
-########################################
-### vim
-########################################
-
-# Instala o Vundle
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-# Baixa o .vimrc
-download https://raw.githubusercontent.com/sergiokopplin/setups/master/.vimrc ~/.vimrc
-
-# Cria a pasta de temas
-mkdir ~/.vim/colors/
-
-# Entra na pasta de temas
-cd ~/.vim/colors
-
-# Baixa o colorscheme dracula
-download https://raw.githubusercontent.com/zenorocha/dracula-theme/master/vim/colors/dracula.vim
-
-# Instala os plugins
-vim +PluginInstall +qall
-
-# Limpa o console
-clear
+sh ./vim.sh
