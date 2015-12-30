a44958bca2c2233d1f6bf6f89683bcf2b759e06b
Sérgio A. Kopplin <sergiokopplin@gmail.com>
2015-12-30 11:46:51 -0200

    Update osx.sh

--- osx.sh
+++ osx.sh
@@ -264,6 +264,5 @@ defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
 # Disable local Time Machine backups
 hash tmutil &> /dev/null && sudo tmutil disablelocal
 
-
 # Restarting
 sudo shutdown -r now
