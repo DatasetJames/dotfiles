23c0f47fe68219616548f9c9ebe8d9b9fe00e0be
Sérgio A. Kopplin <sergiokopplin@gmail.com>
2015-12-30 11:44:52 -0200

    Update vim.sh

--- vim.sh
+++ vim.sh
@@ -1,7 +1,3 @@
-########################################
-### vim
-########################################
-
 # Instala o Vundle
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
