b7f0c195bbf221d3abb8553e76aa7209fb2062ce
Sérgio A. Kopplin <sergiokopplin@gmail.com>
2015-12-30 12:15:22 -0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,3 @@
-# Verifica se tem Git
-command -v git >/dev/null 2>&1 || { echo >&2 "I require git but it's not installed.  Aborting."; exit 1; }
-
-# Função para baixar
-download() {
-    if hash wget 2>/dev/null; then
-        wget "$@"
-    else
-        curl -O "$@"
-    fi
-}
-
 # configs do sistema
 sh osx.sh
 
