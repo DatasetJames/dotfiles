2a268e35c78c39c055490609c9dfe9a06a274bf2
Sérgio A. Kopplin <sergiokopplin@gmail.com>
2016-01-14 20:15:32 -0200

    Update catho.sh

--- catho.sh
+++ catho.sh
@@ -2,4 +2,4 @@ adicionar ambiente php 4 no login items do user
 adicionar variaveis de ambiente no login
 .bash_profile
 .profile
-adicionar layout no iterm
+adicionar layout no iterm eplo save window
