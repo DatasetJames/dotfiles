cbddb939bfce13d5ab33aa38caad2bb5a934dcb5
Sérgio A. Kopplin <sergiokopplin@gmail.com>
2016-01-15 13:53:39 -0200

    Update softwares.sh

--- softwares.sh
+++ softwares.sh
@@ -6,12 +6,13 @@
 # instalar o homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+# instalar homebrew-cask
+brew tap phinze/homebrew-cask
+brew install brew-cask
+
 # utils
 brew install coreutils moreutils findutils ccat
 brew install wget --enable-iri
-
-# Quick Look Plugins (https://github.com/sindresorhus/quick-look-plugins)
-brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql suspicious-package && qlmanage -r
 
 # Spacemacs
 brew tap railwaycat/emacsmacport
@@ -24,20 +25,25 @@ brew cask alfred link
 # git e node
 brew install git node
 
-# instalar homebrew-cask
-brew tap phinze/homebrew-cask
-brew install brew-cask
+# Quick Look Plugins (https://github.com/sindresorhus/quick-look-plugins)
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql suspicious-package && qlmanage -r
 
 # instalar via cask
 brew cask install dash xbox360-controller-driver alfred android-file-transfer atom dropbox firefox google-chrome google-drive imageoptim iterm2 microsoft-office ntfs-free skype spectacle spotify steam the-unarchiver utorrent vlc
 
+###############################################################################
+
 # gulp globalmente
 npm install -g gulp
+
+###############################################################################
 
 # jekyll e dependências
 gem install jekyll github-pages
 # if proxy
 sudo gem install --http-proxy http://user:pass@proxy.*****:8180 jekyll --no-rdoc --no-ri
+
+###############################################################################
 
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
