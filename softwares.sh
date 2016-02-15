0ccc6f4b2fd6b6b113bbaa2e0364be854be39da8
Sérgio Kopplin <sergiokopplin@gmail.com>
2016-02-15 04:21:12 -0800

    Update softwares.sh

--- softwares.sh
+++ softwares.sh
@@ -1,15 +1,10 @@
-
-###############################################################################
-# Softwares                                                                   #
-###############################################################################
-
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-# instalar o homebrew
+# homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# instalar homebrew-cask
+# homebrew-cask
 brew tap phinze/homebrew-cask
 brew install brew-cask
 
@@ -17,32 +12,45 @@ brew install brew-cask
 brew install coreutils moreutils findutils ccat
 brew install wget --enable-iri
 brew install tree
+brew install git node
 
-# Spacemacs
+# spacemacs
 http://emacsformacosx.com/
 brew tap railwaycat/emacsmacport
 brew install emacs-mac --with-spacemacs-icon
 git clone --recursive https://github.com/syl20bnr/spacemacs ~/.emacs.d
 brew linkapps
-
-# Link Cask Apps to Alfred
 brew cask alfred link
 
-# git e node
-brew install git node
-
-# Quick Look Plugins (https://github.com/sindresorhus/quick-look-plugins)
+# quick look plugins (https://github.com/sindresorhus/quick-look-plugins)
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql suspicious-package && qlmanage -r
 
-# instalar via cask
-brew cask install dash mou xbox360-controller-driver alfred android-file-transfer atom dropbox firefox google-chrome google-drive imageoptim iterm2 microsoft-office ntfs-free skype spectacle spotify steam the-unarchiver utorrent vlc
+toinstall=(
+  "dash"
+  "xbox360-controller-driver"
+  "alfred"
+  "android-file-transfer"
+  "atom"
+  "firefox"
+  "google-chrome"
+  "imageoptim"
+  "iterm2"
+  "ntfs-free"
+  "skype"
+  "spectacle"
+  "spotify"
+  "steam"
+  "the-unarchiver"
+  "utorrent"
+  "vlc"
+)
 
-###############################################################################
+# cask
+brew cask install $toinstall
 
-# gulp globalmente
+# gulp
 npm install -g gulp
-# for spacemacs javascript
-npm install -g tern js-beautify jshint
+npm install -g diff-so-fancy
 
 
 # jekyll e dependências
