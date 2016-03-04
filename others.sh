f64cffb52fa0f46f19da3e8e2feecfcfaa1aea87
Sérgio Kopplin <sergiokopplin@gmail.com>
2016-03-04 12:01:51 -0300

    Update and rename softwares.sh to others.sh

--- others.sh
+++ others.sh
@@ -1,9 +1,6 @@
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-# gulp
-npm install -g gulp
-
 # fancy diff
 npm install -g diff-so-fancy
 brew install gnu-sed
@@ -13,6 +10,9 @@ git config --global color.diff-highlight.newNormal "green bold"
 git config --global color.diff-highlight.newHighlight "green bold 22"
 git config --global pager.diff "diff-so-fancy | less --tabs=1,5 -RFX"
 git config --global pager.show "diff-so-fancy | less --tabs=1,5 -RFX"
+
+# gulp
+npm install -g gulp
 
 # jekyll
 jekyllinstall=(
@@ -44,6 +44,8 @@ https://gist.github.com/sergiokopplin/a129217149085348f463
 # rescuetime
 https://www.rescuetime.com/get_rescuetime
 
+# helpers
 npm install -g how2
 
+# study
 npm install webpack -g
