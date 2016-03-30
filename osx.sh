12dc2b5e87e9b2709793b2bb6ceabdb70d93fda8
Sérgio Kopplin <sergiokopplin@gmail.com>
2016-03-30 18:45:24 -0300

    Update osx.sh

--- osx.sh
+++ osx.sh
@@ -268,3 +268,7 @@ hash tmutil &> /dev/null && sudo tmutil disablelocal
 
 # Hidden files
 defaults write com.apple.finder AppleShowAllFiles YES
+
+# Key repeat
+defaults write -g InitialKeyRepeat -int 1 # normal minimum is 15 (225 ms)
+defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
