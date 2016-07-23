892e7c4e38381e2b700bd0119fc202e27642e2f4
Sérgio A. Kopplin <sergiokopplin@gmail.com>
2016-07-23 12:50:04 -0300

    Update others.sh

--- others.sh
+++ others.sh
@@ -13,41 +13,4 @@ git config --global pager.show "diff-so-fancy | less --tabs=1,5 -RFX"
 
 # gulp
 npm install -g gulp
-
-# jekyll
-jekyllinstall=(
-  "github-pages"
-  "bundler"
-  "rake"
-  "pygments.rb"
-  "jekyll"
-  "html-proofer"
-  "jemoji"
-  "jekyll-mentions"
-  "jekyll-seo-tag"
-  "jekyll-feed"
-  "jekyll-sitemap"
-  "jekyll-gist"
-)
-
-gem install $jekyllinstall --no-rdoc --no-ri
-
-# bemean
-npm install -g mongo-hacker nodemon
-postman for chrome
-
-# for hackintosh
-https://gist.github.com/sergiokopplin/a129217149085348f463
-
-# helpers
-npm install -g how2
-
-https://github.com/paulyoung/Statusfy
-
-https://github.com/tjunnone/npm-check-updates
-
-# dash
-
 # stremio
-
-# deco ide
