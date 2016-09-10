590130071c0db4980c2f0a80b4c359a7b6455a95
Sérgio A. Kopplin <sergiokopplin@gmail.com>
2016-09-10 09:15:25 -0300

    Update software.sh

--- software.sh
+++ software.sh
@@ -12,6 +12,7 @@ brew cask install firefox
 brew cask install google-chrome
 brew cask install iterm2
 brew cask install skype
+brew cask install slack
 brew cask install spectacle
 brew cask install spotify
 brew cask install steam
