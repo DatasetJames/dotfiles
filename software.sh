778dbb28d4be6739af13cfe9e9b33a3288cf0b5c
Sérgio A. Kopplin <sergiokopplin@gmail.com>
2017-06-01 12:47:53 -0700

    Update software.sh

--- software.sh
+++ software.sh
@@ -1,40 +1,30 @@
 # homebrew and apps
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew tap phinze/homebrew-cask
-brew install brew-cask ccat git node wget
+brew install brew-cask ccat node wget
 
 # cask
 brew cask install xbox360-controller-driver
 brew cask install alfred
 brew cask install android-file-transfer
-brew cask install visual-studio-code
 brew cask install atom
 brew cask install firefox
 brew cask install google-chrome
 brew cask install iterm2
-brew cask install hyper
 brew cask install skype
 brew cask install slack
 brew cask install spectacle
 brew cask install spotify
-brew cask install steam
 brew cask install the-unarchiver
 brew install zsh-syntax-highlighting
 
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-# npm updates
-npm install -g npm-check-updates
-
-# better prompt
-npm install -g pure-prompt
-
-# start static server
-npm install -g node-static 
+# npm
+npm install -g npm-check-updates yarn node-static diff-so-fancy
 
 # fancy diff
-npm install -g diff-so-fancy
 brew install gnu-sed
 git config --global color.diff-highlight.oldNormal "red bold"
 git config --global color.diff-highlight.oldHighlight "red bold 52"
@@ -42,6 +32,3 @@ git config --global color.diff-highlight.newNormal "green bold"
 git config --global color.diff-highlight.newHighlight "green bold 22"
 git config --global pager.diff "diff-so-fancy | less --tabs=1,5 -RFX"
 git config --global pager.show "diff-so-fancy | less --tabs=1,5 -RFX"
-
-# mac hide bar
-http://matthewpalmer.net/vanilla/
